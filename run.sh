11b991bc251ac6e0d63fba0d7f8d88a2c996ef5d
riska <162248347+riskanata@users.noreply.github.com>
2024-03-23 05:43:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-abef2e0f-15dd-4c7a-9b34-6b6ffc1e9324/)
 
 
 echo "$curl_output" >> Update.tsx
